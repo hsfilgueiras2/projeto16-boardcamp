c31ff8b08714dd4458840c8af82283638f7473cd
Heitor Filgueiras <hsfilgueiras2@gmail.com>
2022-12-13 16:06:21 -0300

    feat: post get rentals

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import pkg from 'pg';
 import * as dotenv from 'dotenv'
+import dayjs from 'dayjs';
 dotenv.config()
 
 const { Pool } = pkg;
@@ -78,6 +79,68 @@ app.put("/customers/:id",async (req, res)=>{
     const customers = await connection.query('UPDATE customers SET name=$1, phone=$2,cpf=$3,birthday=$4 WHERE id=$5',
     [body.name, body.phone,body.cpf,body.birthday,customerId])
     res.sendStatus(201)
+    }catch(err){console.log(err);res.sendStatus(500)}
+})
+app.get("/rentals",async(req,res)=>{
+    const customerId = req.query.customerId || "";
+    const gameId = req.query.gameId || "";
+    try{
+        const categories = await (await connection.query('SELECT * FROM categories')).rows
+        const customers = await (await connection.query('SELECT * FROM customers')).rows
+        const games = await (await connection.query('SELECT * FROM games')).rows
+        let rentals
+        if(gameId != "" && customerId != "")
+        {
+            rentals = await (await connection.query('SELECT * FROM rentals WHERE '+
+            'customerId = $1 AND gameId = $2',[customerId, gameId])).rows
+        }
+        else if (gameId != "" && customerId == ""){
+            rentals = await (await connection.query('SELECT * FROM rentals WHERE '+
+            'gameId = $1',[gameId])).rows
+        }
+        else if(gameId == "" && customerId != "")
+        {
+            rentals = await (await connection.query('SELECT * FROM rentals WHERE '+
+            'customerId = $1',[customerId])).rows 
+        }
+        else{
+            rentals = await (await connection.query('SELECT * FROM rentals')).rows 
+        }
+        rentals = rentals.map((rental)=>{
+            const game = games.find(obj=>{return obj.id = rental.gameId})
+            const customer = customers.find(obj=>{return obj.id = rental.customerId})
+            const category = categories.find(obj=>{return obj.id = game.categoryId})
+            return {...rental,
+            customer:{
+                id:customer.id,
+                name:customer.name
+            },
+            game:{
+                id:game.id,
+                name:game.name,
+                categoryId: game.categoryId,
+                categoryName:category.name
+            }}
+
+        })
+        res.send(rentals);
+
+    }catch(err){console.log(err);res.sendStatus(500)}
+})
+app.post("/rentals", async(req,res)=>{
+    const body = req.body
+    console.log(body)
+    const rentDate = dayjs().format('YYYY-MM-DD')
+    try{
+        const dailyPrice = await (await connection.query('SELECT games."pricePerDay" FROM games WHERE id=$1',[body.gameId])).rows[0].pricePerDay
+        console.log(dailyPrice)
+        const originalPrice = dailyPrice * body.daysRented;
+        console.log(originalPrice)
+        const rentals = await connection.query('INSERT INTO rentals '+
+        '("customerId","gameId","rentDate","daysRented","returnDate","originalPrice","delayFee")'+
+        'VALUES ($1,$2,$3,$4,$5,$6,$7)',
+        [body.customerId, body.gameId, rentDate, body.daysRented, null, originalPrice, null])
+        res.sendStatus(201)
     }catch(err){console.log(err);res.sendStatus(500)}
 })
 
